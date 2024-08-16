f2a8bbb57deb4683c8c6822b4ece087ede0a5765
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-08-16 10:38:51 +0300

    rename data dump and add solution to task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """The function should return a tuple of size two containing a start
     index and an end index corresponding to the range of indexes to
     return in a list for those particular pagination parameters.
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Task 1 Module"""
+
+
+import csv
+import math
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """The function should return a tuple of size two containing a start
+    index and an end index corresponding to the range of indexes to
+    return in a list for those particular pagination parameters.
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return paginated dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        data = self.dataset()
+        
+        try:
+            start, end = index_range(page, page_size)
+            return data[start: end]
+        except IndexError:
+            return []
