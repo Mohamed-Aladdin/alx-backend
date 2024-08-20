8167eab8d36158ef8933bd69525b79287e622040
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-08-20 19:38:17 +0300

    Fix typo in super declaration

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,7 +11,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize
         """
-        super.__init__()
+        super().__init__()
 
     def put(self, key, item):
         """Add an item in the cache
