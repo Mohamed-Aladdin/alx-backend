a0096fd3a40af0fdc3af9a4e9102448f470d6234
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-23 15:52:46 +0300

    Add task 10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,27 @@
+import { Queue, Job, createQueue } from 'kue';
+
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!(jobs instanceof Array)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((item) => {
+    const job = queue.create('push_notification_code_3');
+
+    job
+      .on('enqueue', () => {
+        console.log('Notification job created: ', job.id);
+      })
+      .on('complete', () => {
+        console.log('Notification job ', job.id, ' completed');
+      })
+      .on('failed', (err) => {
+        console.log('Notification job ', job.id, 'failed: ', err.toString());
+      })
+      .on('progress', (progress, _data) => {
+        console.log('Notification job ', job.id, ' ', progress, '% complete');
+      });
+
+    job.save();
+  });
+}
