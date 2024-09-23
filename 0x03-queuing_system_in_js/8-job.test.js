4007b261e84a4888ec135170095182bfeaedb21d
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-23 16:23:27 +0300

    Add task 11

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,90 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { createQueue } from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+  const spy = sinon.spy(console, 'log');
+  const QUEUE = createQueue({ name: 'push_notification_code_test' });
+
+  before(() => {
+    QUEUE.testMode.enter(true);
+  });
+
+  after(() => {
+    QUEUE.testMode.clear();
+    QUEUE.testMode.exit();
+  });
+
+  afterEach(() => {
+    spy.resetHistory();
+  });
+
+  it('adding jobs to the queue', (done) => {
+    expect(QUEUE.testMode.jobs.length).to.equal(0);
+
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account',
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account',
+      },
+    ];
+
+    createPushNotificationsJobs(jobs, QUEUE);
+    expect(QUEUE.testMode.jobs.length).to.equal(2);
+    expect(QUEUE.testMode.jobs[0].data).to.deep.equal(jobs[0]);
+    expect(QUEUE.testMode.jobs[0].type).to.equal('push_notification_code_3');
+
+    QUEUE.process('push_notification_code_3', () => {
+      expect(
+        spy.calledWith('Notification job created:', QUEUE.testMode.jobs[0].id)
+      ).to.be.true;
+      done();
+    });
+  });
+
+  it('registering the process event handler', (done) => {
+    QUEUE.testMode.jobs[0].addListener('progress', () => {
+      expect(
+        spy.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          '25% complete'
+        )
+      ).to.be.true;
+      done();
+    });
+    QUEUE.testMode.jobs[0].emit('progress', 25);
+  });
+
+  it('registering the failed event handler', (done) => {
+    QUEUE.testMode.jobs[0].addListener('failed', () => {
+      expect(
+        spy.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          'failed:',
+          'Failed to send'
+        )
+      ).to.be.true;
+    });
+    QUEUE.testMode.jobs[0].emit('failed', new Error('Failed to send'));
+  });
+
+  it('registering the complete event handler', (done) => {
+    QUEUE.testMode.jobs[0].addListener('complete', () => {
+      expect(
+        spy.calledWith(
+          'Notification job',
+          QUEUE.testMode.jobs[0].id,
+          'completed'
+        )
+      ).to.be.true;
+    });
+    QUEUE.testMode.jobs[0].emit('complete');
+  });
+});
