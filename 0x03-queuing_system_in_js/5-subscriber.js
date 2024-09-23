e67c30c0c35970a588e2e0d082612e153686e3df
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-23 14:36:21 +0300

    Add task 5

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -8,14 +8,13 @@ client.on('error', (err) =>
   console.log('Redis client not connected to the server: ', err.toString())
 );
 
-function publishMessage(message, time) {
-  setTimeout(() => {
-    console.log(`About to send ${message}`);
-    client.publish('holberton school channel', message);
-  }, time);
-}
+client.subscribe('holberton school channel');
 
-publishMessage('Holberton Student #1 starts course', 100);
-publishMessage('Holberton Student #2 starts course', 200);
-publishMessage('KILL_SERVER', 300);
-publishMessage('Holberton Student #3 starts course', 400);
+client.on('message', (_err, msg) => {
+  console.log(msg);
+
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
